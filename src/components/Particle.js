70e40c144f6b94a96e1b41ad124104a4d154dbae
kumarajay0412 <66991625+kumarajay0412@users.noreply.github.com>
2021-02-26 13:48:00 +0530

    Particle

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -6,46 +6,97 @@ function Particle() {
     <Particles
       params={{
         particles: {
-          number: {
-            value: 160,
-            density: {
-              enable: true,
-              value_area: 1500,
-            },
+        number: {
+          value: 140,
+          density: {
+            enable: true,
+            value_area: 800
+          }
+        },
+        color: {
+          value: [ '#2EB67D', '#ECB22E', '#E01E5B', '#36C5F0' ]
+        },
+        shape: {
+          type: 'circle',
+          stroke: {
+            width: 0,
+            color: '#fff'
           },
-          line_linked: {
+          polygon: {
+            nb_sides: 5
+          },
+          image: {
+            src: 'https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png',
+            width: 100,
+            height: 100
+          }
+        },
+        
+        opacity: {
+          value: 1,
+          random: false,
+          anim: {
             enable: false,
-            opacity: 0.03,
-          },
-          move: {
-            direction: "right",
-            speed: 0.05,
-          },
-          size: {
-            value: 1,
-          },
-          opacity: {
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.05,
-            },
-          },
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
         },
-        interactivity: {
-          events: {
-            onclick: {
-              enable: true,
-              mode: "push",
-            },
-          },
-          modes: {
-            push: {
-              particles_nb: 1,
-            },
-          },
+        size: {
+          value: 8,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 10,
+            size_min: 10,
+            sync: false
+          }
         },
-        retina_detect: true,
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: '#808080',
+          opacity: 0.4,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 5,
+          direction: 'none',
+          random: false,
+          straight: false,
+          out_mode: 'out',
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+          }
+        }
+      },
+      interactivity: {
+        detect_on: 'window',
+        events: {
+          onhover: {
+            enable: true,
+            mode: 'repulse'
+          },
+          onclick: {
+            enable: true,
+            mode: 'push'
+          }
+        },
+        modes: {
+          'repulse' : {
+            distance: 70,
+            duration: 0.4
+          },
+          'push' : {
+            particles_nb: 4
+          }
+        }
+      },
+      retina_detect: true
       }}
     />
   );
